5c7e3a93a1a9118c9d2ab8c26c8755a167a17812
ROYYYDS <13770559934@163.com>
2023-05-28 13:12:19 +0100

    Try single html for one storyline, haven't finished

--- storyline/car/go_where.js
+++ storyline/car/go_where.js
@@ -1,5 +1,19 @@
 'use strick';
 
+function changeBackground(image) {
+    // this fucntion can a backgroud iamge
+    const container = document.querySelector('.container');
+    container.style.backgroundImage = `url(${image})`;
+}
+
+function showPage(page) {
+    const new_page = document.querySelector(`#${page}`)
+    // show page(div) with spsific id, id is the input
+    new_page.style.display = 'block';
+}
+
+showPage("go_where")
+
 document.querySelector('.left').addEventListener('click', function () {
     window.location.assign("thief.html")
 });
