e8932f8a9e2fdb416fb158c6bdb0fb82fe28f324
hc1321 <hannacoutts@gmail.com>
2023-06-08 19:24:14 +0100

    Revert "Adding Django Code - integration still needed"
    This reverts commit 4066501d87ae2e7d2582d1aa867928923c50e92f.

--- DAPP/Choose_and_Tell/static/Choose_and_Tell/settings.js
+++ DAPP/Choose_and_Tell/static/Choose_and_Tell/settings.js
@@ -1,49 +1,32 @@
 document.addEventListener('DOMContentLoaded', function(){
     const contrast = document.querySelector('#contrast_control');
+    const bold_control = document.querySelector('#bold_control')
     const body = document.body;
-
+    
+    //ADJUSTING CONTRAST
     contrast.addEventListener('input', function() {
         const contrast_value = contrast.value;
-        const new_contrast = (100-contrast_value) / 100;
         
-        if (contrast_value == "0"){
-            body.style.filter= "none";
-        } else {
-            body.style.filter = `brightness(${new_contrast})invert(${contrast_value}%)`;
-        }
+        body.style.filter = `contrast(${contrast_value}%)`;
     });
 
-
-
-    
-//for space station
-
-window.onpopstate = (function() {
-    document.querySelector('#destination_body').style.display = 'block';
-    document.querySelector('#space-station-chosen').style.display = 'none';
-  });
-        
-//document.querySelector('#center_destination').addEventListener('click', () => {
- //   document.querySelector('#destination_body').style.display = 'none';
-   // document.querySelector('#space-station-chosen').style.display = 'block';
-  //  });
-
-
-    //Choose destination javascript
-    
-    document.querySelector('.left').addEventListener('click', function () {
-        window.location.replace("Travelling to destination mars.html")
-    });
-    
-    document.querySelector('.right').addEventListener('click', function () {
-        window.location.replace("Travelling to destination moon.html")
-    });
-    
-    document.querySelector('.center').addEventListener('click', function () {
-        window.location.replace("Travelling to destination space-station.html")
-    });
+    //ADJUSTING BOLDNESS
+    bold_control.addEventListener('input', function() {
+        const bold_value = bold_control.value;
+        const font_weight = FontAdjust(bold_value);
+        body.style.fontWeight = font_weight;
+    })
 });
 
+function FontAdjust(slider) {
+    const minSlider = 0;
+    const maxSlider = 200;
+    const minFontWeight = 200;
+    const maxFontWeight = 1000;
 
+    const normalized = (slider - minSlider) / (maxSlider - minSlider);
+    const mappedFontWeight = Math.round(minFontWeight + (normalized * (maxFontWeight - minFontWeight)));
+  
+    return mappedFontWeight;
 
-
+}
