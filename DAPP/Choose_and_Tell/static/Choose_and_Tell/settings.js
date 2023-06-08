9dfbd2697823f131ad1341473417b9842f852164
hc1321 <hannacoutts@gmail.com>
2023-06-08 23:12:35 +0100

    Home button and framework for text size adjust

--- DAPP/Choose_and_Tell/static/Choose_and_Tell/settings.js
+++ DAPP/Choose_and_Tell/static/Choose_and_Tell/settings.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', function(){
     const contrast = document.querySelector('#contrast_control');
     const bold_control = document.querySelector('#bold_control')
     const body = document.body;
+    const headings = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
+    const text_size = document.querySelector('#text_size_control');
     
     //ADJUSTING CONTRAST
     contrast.addEventListener('input', function() {
@@ -15,7 +17,24 @@ document.addEventListener('DOMContentLoaded', function(){
         const bold_value = bold_control.value;
         const font_weight = FontAdjust(bold_value);
         body.style.fontWeight = font_weight;
-    })
+        for (const heading of headings) {
+            heading.style.fontWeight = font_weight;
+        }
+    });
+
+    //ADJUSTING TEXT SIZE (NOT FINISHED)
+    text_size.addEventListener('input', function() {
+        const size = text_size.value;
+        const size_scale = SizeAdjust(size);
+
+        const allelements = document.querySelectorAll('*');
+        allelements.forEach(function(element) {
+            const original_font_size = window.getComputedStyle(element).fontSize;
+            const adjusted_font_size = parseFloat(original_font_size)*size_scale;
+            element.style.fontSize = adjusted_font_size + 'px';
+        });
+    });
+
 });
 
 function FontAdjust(slider) {
@@ -24,9 +43,13 @@ function FontAdjust(slider) {
     const minFontWeight = 200;
     const maxFontWeight = 1000;
 
-    const normalized = (slider - minSlider) / (maxSlider - minSlider);
-    const mappedFontWeight = Math.round(minFontWeight + (normalized * (maxFontWeight - minFontWeight)));
+    const normalised = (slider - minSlider) / (maxSlider - minSlider);
+    const mappedFontWeight = Math.round(minFontWeight + (normalised * (maxFontWeight - minFontWeight)));
   
     return mappedFontWeight;
+}
 
+function SizeAdjust(slider){
+    SliderScale = slider/20;
+    return SliderScale;
 }
